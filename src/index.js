9de14b0181231620628907a37e599c24ee1b96c0
alisson-lucas <lucasoliveira.alisson@gmail.com>
2020-06-10 00:11:42 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {StatusBar} from 'react-native';
+
+
+import Navigation from './navigation';
+
+export default function App() {
+  return (
+    <>
+    <StatusBar barStyle = 'light-content' backgroundColor = '#fff' />
+    <NavigationContainer>
+        <Navigation />
+    </NavigationContainer>
+    </>
+  );
+}
+
+
---
+++ src/pages/Home/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {  Text  } from 'react-native';
+import { Wrapper, Header, BalanceContainer, BalanceTitle, Balance, Container } from './styles';
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons'
+
+export default function Home() {
+    return (
+        <Wrapper>
+            <Container>
+                <Header>
+                    <MaterialCommunityIcons name = 'qrcode-scan' size = {30} color = '#10c86e' />
+
+                <BalanceContainer>
+                    <BalanceTitle>Meu saldo</BalanceTitle>
+                    <Balance>0,00</Balance>
+                </BalanceContainer>
+
+                    <AntDesign name = 'gift' size = {30} color = '#10c86e' />
+                </Header>
+            </Container>
+        </Wrapper>
+        );
+}
