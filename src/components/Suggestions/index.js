36a18eed61e0374c9472a4eac3399760963eea99
alisson-lucas <lucasoliveira.alisson@gmail.com>
2020-06-15 00:05:13 -0300

    Tela inicial finalizada

--- src/components/Suggestions/index.js
+++ src/components/Suggestions/index.js
@@ -15,22 +15,22 @@ const items = [
     {
         key: String(Math.random()),
         img: img1,
-        label: 'Doações'
+        label: 'Recarga'
     },
     {
         key: String(Math.random()),
         img: img2,
-        label: 'Doações'
+        label: 'Uber'
     },
     {
         key: String(Math.random()),
         img: img3,
-        label: 'Doações'
+        label: 'ônibus'
     },
     {
         key: String(Math.random()),
         img: img4,
-        label: 'Doações'
+        label: 'Tv'
     },
     {
         key: String(Math.random()),
@@ -40,12 +40,12 @@ const items = [
     {
         key: String(Math.random()),
         img: img6,
-        label: 'Doações'
+        label: 'Barras'
     },
     {
         key: String(Math.random()),
         img: img7,
-        label: 'Doações'
+        label: 'Faq'
     }
 ];
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,6 +5,8 @@ import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons'
 
 import Suggestions from '../../components/Suggestions'
 import Activities from '../../components/Activities'
+import Tips from '../../components/Tips'
+import Banner from '../../components/Banner'
 
 export default function Home() {
     return (
@@ -23,6 +25,8 @@ export default function Home() {
                 
                 <Suggestions />
                 <Activities />
+                <Tips />
+                <Banner />
             </Container>
         </Wrapper>
     );
